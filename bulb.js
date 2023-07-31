f7df4567c21b091095b987c4dfc9b30b49989ac8
Tharun <138850352+tharunt415@users.noreply.github.com>
2023-07-31 16:14:23 +0530

    Add files via upload

---
+++ bulb.js
@@ -0,0 +1,8 @@
+function on(){
+    document.getElementById('img').src='https://previews.123rf.com/images/murika/murika1511/murika151100069/48123160-bright-glowing-incandescent-light-bulb-on-a-white-background.jpg'
+
+}
+function off(){
+    document.getElementById('img').src='https://previews.123rf.com/images/ericmilos/ericmilos0912/ericmilos091200136/6109526-3d-render-of-light-bulb-on-white.jpg'
+    
+}
